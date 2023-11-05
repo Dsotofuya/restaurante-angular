f4d1d48fe271d04511d370a3c1e053d44ffa0ace
Daniel Soto Fuya <dsotofuya@gmail.com>
2023-11-05 18:47:28 -0500

    Home module, Add Categoria component

--- src/app/home/components/add-categoria/add-categoria.component.ts
+++ src/app/home/components/add-categoria/add-categoria.component.ts
@@ -1,11 +1,28 @@
-import { Component } from '@angular/core';
-
+import { Component, ViewChild } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-add-categoria',
   templateUrl: './add-categoria.component.html',
   styleUrls: ['./add-categoria.component.css']
 })
-export class AddCategoriaComponent {
 
+export class AddCategoriaComponent {
+  nombreCategoria: string = '';
+  @ViewChild ('bio') bio : any;
+
+  constructor(private http : HttpClient){
+    console.log(this.bio);
+  }
+
+  addCategoria(event: Event){
+    event.preventDefault();
+    this.http.post('http://localhost:8081/restaurante/categoria/add', {
+      "nombre": this.nombreCategoria,
+    }).subscribe({next: (data) => {
+      alert(data);
+    }, error: (error) => {
+      alert(error);
+    }})
+  }
 }
--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -1,8 +1,9 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
+import { FormsModule } from '@angular/forms';
 import { HomeRoutingModule } from './home-routing.module';
 import { AddCategoriaComponent } from './components/add-categoria/add-categoria.component';
+import { HttpClientModule } from '@angular/common/http';
 
 
 @NgModule({
@@ -10,6 +11,8 @@ import { AddCategoriaComponent } from './components/add-categoria/add-categoria.
     AddCategoriaComponent
   ],
   imports: [
+    HttpClientModule,
+    FormsModule,
     CommonModule,
     HomeRoutingModule
   ]
